f7edcfae2986011e7b694ed7086ce33304e799b1
Boitumelo <boitumelolefophane@gmail.com>
2024-10-30 17:35:46 +0200

    chapter1 - chapter3

---
+++ chapter03/src/Products.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Products() {
+  const products = ["Learning React", "Pro React", "Beginning React"];
+  const listProducts = products.map((product) => (
+    <li key={product.toString()}>{product}</li>
+  ));
+  return (
+    <div>
+      <ul>{listProducts}</ul>
+    </div>
+  );
+}
+
+// const Products = () => {
+//   return (
+//     <div>
+//       <h2> Products</h2>
+//       <ul>{listProducts}</ul>
+//     </div>
+//   );
+//};
+
+export default Products;
